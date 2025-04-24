349ab8c2ffd6b58711d60e69ccaa66aef0dab62d
Henry Matthews <hm110896@gmail.com>
2025-04-24 11:27:07 -0400

    created auth-site template

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+    'url' : `mongodb+srv://henryamatsu:Vw0Aml0jLB6fOc1A@cluster0.aejyj1h.mongodb.net/secret-gold?retryWrites=true&w=majority&appName=Cluster0`, 
+    'dbName': 'secret-gold'
+};
+
+
+// module.exports = {
+//     'url' : `mongodb+srv://henryamatsu:${process.env.MONGODB_PASSKEY}@cluster0.aejyj1h.mongodb.net/secret-gold?retryWrites=true&w=majority&appName=Cluster0`, 
+//     'dbName': 'secret-gold'
+// };
